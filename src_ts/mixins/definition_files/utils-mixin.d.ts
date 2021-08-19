1c1f96a5d1991b2954c82293a7c59d2f8fdb0ad2
Adriana Trif <trif.corina.adriana@gmail.com>
2021-08-19 10:15:18 +0300

    backup d.ts

---
+++ src_ts/mixins/definition_files/utils-mixin.d.ts
@@ -0,0 +1,29 @@
+import { Constructor } from '@unicef-polymer/etools-types';
+import { LitElement } from 'lit-element';
+declare function UtilsMixin<T extends Constructor<LitElement>>(
+  baseClass: T
+): {
+  new (...args: any[]): {
+    _equals(a: any, b: any): boolean;
+    _toNumber(val: any): number;
+    _formatNumber(
+      val: any,
+      placeholder: any,
+      decimals: any,
+      thousandsPoint: any,
+      decimalsPoint?: any
+    ): any;
+    capitalizeFirstLetter(text: string): string;
+    _deferred(): any;
+    _toPercentage(value: any): any;
+    _fieldsAreValid(): boolean;
+    _ternary(value: any, expected: any, value1: any, value2: any): any;
+    _withDefault(value: any, defaultValue: any): any;
+    _formatIndicatorValue(
+      displayType: string,
+      value: any,
+      percentize: boolean
+    ): any;
+  };
+} & T;
+export default UtilsMixin;
