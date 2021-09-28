4cfb69bb10d3faeb411417df8c9d8f6473904474
Adriana Trif <trif.corina.adriana@gmail.com>
2021-09-28 19:26:52 +0300

    eslint, prettier config

--- src_ts/utils/validation-helper.ts
+++ src_ts/utils/validation-helper.ts
@@ -1,19 +1,15 @@
-import { LitElement } from 'lit-element';
-import { PolymerElement } from '@polymer/polymer';
-import { AnyObject } from '@unicef-polymer/etools-types';
+import {LitElement} from 'lit-element';
+import {PolymerElement} from '@polymer/polymer';
+import {AnyObject} from '@unicef-polymer/etools-types';
 
 type ValidatableElement = (LitElement | PolymerElement) & {
   validate(): boolean;
 };
 
-export const validateRequiredFields = (
-  element: LitElement | PolymerElement
-) => {
+export const validateRequiredFields = (element: LitElement | PolymerElement) => {
   let isValid = true;
   element!
-    .shadowRoot!.querySelectorAll<ValidatableElement>(
-      '[required]:not([readonly]):not([hidden])'
-    )
+    .shadowRoot!.querySelectorAll<ValidatableElement>('[required]:not([readonly]):not([hidden])')
     .forEach((el) => {
       if (el && el.validate && !el.validate()) {
         isValid = false;
@@ -23,20 +19,14 @@ export const validateRequiredFields = (
 };
 
 export const resetRequiredFields = (element: LitElement | PolymerElement) => {
-  element!
-    .shadowRoot!.querySelectorAll<ValidatableElement>('[required]')
-    .forEach((el: AnyObject) => {
-      if (el) {
-        el.invalid = false;
-      }
-    });
+  element!.shadowRoot!.querySelectorAll<ValidatableElement>('[required]').forEach((el: AnyObject) => {
+    if (el) {
+      el.invalid = false;
+    }
+  });
 };
 
-export const fieldValidationReset = (
-  element: LitElement | PolymerElement,
-  selector: string,
-  useValidate?: boolean
-) => {
+export const fieldValidationReset = (element: LitElement | PolymerElement, selector: string, useValidate?: boolean) => {
   if (!useValidate) {
     useValidate = false;
   }
