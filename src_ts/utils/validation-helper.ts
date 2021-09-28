e072b564a736313c4bf82b80eef645f9c7ba23dd
Adriana Trif <trif.corina.adriana@gmail.com>
2021-09-28 18:46:09 +0300

    Validation tweak

--- src_ts/utils/validation-helper.ts
+++ src_ts/utils/validation-helper.ts
@@ -1,32 +1,48 @@
-import {LitElement} from 'lit-element';
-import {PolymerElement} from '@polymer/polymer';
-import {AnyObject} from '@unicef-polymer/etools-types';
+import { LitElement } from 'lit-element';
+import { PolymerElement } from '@polymer/polymer';
+import { AnyObject } from '@unicef-polymer/etools-types';
 
-type ValidatableElement = (LitElement | PolymerElement) & {validate(): boolean};
+type ValidatableElement = (LitElement | PolymerElement) & {
+  validate(): boolean;
+};
 
-export const validateRequiredFields = (element: LitElement | PolymerElement) => {
+export const validateRequiredFields = (
+  element: LitElement | PolymerElement
+) => {
   let isValid = true;
-  element!.shadowRoot!.querySelectorAll<ValidatableElement>('[required]').forEach((el) => {
-    if (el && el.validate && !el.validate()) {
-      isValid = false;
-    }
-  });
+  element!
+    .shadowRoot!.querySelectorAll<ValidatableElement>(
+      '[required]:not([readonly])'
+    )
+    .forEach((el) => {
+      if (el && el.validate && !el.validate()) {
+        isValid = false;
+      }
+    });
   return isValid;
 };
 
 export const resetRequiredFields = (element: LitElement | PolymerElement) => {
-  element!.shadowRoot!.querySelectorAll<ValidatableElement>('[required]').forEach((el: AnyObject) => {
-    if (el) {
-      el.invalid = false;
-    }
-  });
+  element!
+    .shadowRoot!.querySelectorAll<ValidatableElement>('[required]')
+    .forEach((el: AnyObject) => {
+      if (el) {
+        el.invalid = false;
+      }
+    });
 };
 
-export const fieldValidationReset = (element: LitElement | PolymerElement, selector: string, useValidate?: boolean) => {
+export const fieldValidationReset = (
+  element: LitElement | PolymerElement,
+  selector: string,
+  useValidate?: boolean
+) => {
   if (!useValidate) {
     useValidate = false;
   }
-  const field = element.shadowRoot!.querySelector(selector) as LitElement & {validate(): boolean};
+  const field = element.shadowRoot!.querySelector(selector) as LitElement & {
+    validate(): boolean;
+  };
   if (field) {
     if (useValidate) {
       field.validate();
