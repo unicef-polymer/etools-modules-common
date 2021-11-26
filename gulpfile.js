3af00f35af5ac21845a58f8a6db2e4632492c5bd
Adriana Trif <trif.corina.adriana@gmail.com>
2021-11-26 17:46:31 +0200

    Config ci linting job

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const gulp = require('gulp');
 const through2 = require('through2').obj;
 const path = require('path');
 const ts = require('gulp-typescript');
-const tsProject = ts.createProject('tsconfig.json', { noEmitOnError: false });
+const tsProject = ts.createProject('tsconfig.json', {noEmitOnError: false});
 
 const SRC = 'src_ts/';
 const DIST = 'dist/';
@@ -17,73 +18,82 @@ const DEPS_PARSING_REG = /([^(]+?)\(/g;
 const createDepsImportReg = (depsName) => new RegExp(`import\\s[^;]*?${depsName}\\s+[^;]*?;`, 's');
 const dependenciesMap = {};
 
-gulp.task('default', () => gulp.src(`${SRC}/**/*.ts`)
-    .pipe(through2((file, enc, callback) => {
+gulp.task('default', () =>
+  gulp
+    .src(`${SRC}/**/*.ts`)
+    .pipe(
+      through2((file, enc, callback) => {
         if (isMixin(file)) {
-            prepareAndMapFile(file);
+          prepareAndMapFile(file);
         }
         callback(null, file);
-    }))
+      })
+    )
     .pipe(tsProject())
-    .on('error', () => { /* Ignore compiler errors */})
-    .pipe(through2((file, enc, callback) => {
+    .on('error', () => {
+      /* Ignore compiler errors */
+    })
+    .pipe(
+      through2((file, enc, callback) => {
         // check if file exists in global map object
         const filePath = path.relative(`${__dirname}/${DIST}`, file.path);
         if (dependenciesMap[filePath]) {
-            fixDeclarationFile(file, filePath);
+          fixDeclarationFile(file, filePath);
         }
         callback(null, file);
-    }))
-    .pipe(gulp.dest(DIST)));
+      })
+    )
+    .pipe(gulp.dest(DIST))
+);
 
 function isMixin(file) {
-    if (!file.isBuffer() || !file.path.includes('mixins')) {
-        return false;
-    }
+  if (!file.isBuffer() || !file.path.includes('mixins')) {
+    return false;
+  }
 
-    //check if this is mixin
-    const fileContent = String(file.contents);
-    return new RegExp(LIT_CONSTRUCTOR).test(fileContent);
+  // check if this is mixin
+  const fileContent = String(file.contents);
+  return new RegExp(LIT_CONSTRUCTOR).test(fileContent);
 }
 
 function prepareAndMapFile(file) {
-    // set constructor to any
-    const replacedContent = String(file.contents).replace(LIT_CONSTRUCTOR, ANY_CONSTRUCTOR);
-    file.contents = Buffer.from(replacedContent);
+  // set constructor to any
+  const replacedContent = String(file.contents).replace(LIT_CONSTRUCTOR, ANY_CONSTRUCTOR);
+  file.contents = Buffer.from(replacedContent);
 
-    // check if other mixins are used inside mixin
-    const [, dependencies = ''] = replacedContent.match(DEPENDENCY_REG) || []; // ends up with SomeMixin(OtherMixin(BaseClass))
-    const parsed = Array.from(dependencies.matchAll(DEPS_PARSING_REG))
-        .map(([, name]) => {
-            // find import path for dependency
-            const reg = createDepsImportReg(name);
-            const [importLine] = replacedContent.match(reg);
-            return { name, importLine };
-        });
+  // check if other mixins are used inside mixin
+  // eslint-disable-next-line max-len
+  const [, dependencies = ''] = replacedContent.match(DEPENDENCY_REG) || []; // ends up with SomeMixin(OtherMixin(BaseClass))
+  const parsed = Array.from(dependencies.matchAll(DEPS_PARSING_REG)).map(([, name]) => {
+    // find import path for dependency
+    const reg = createDepsImportReg(name);
+    const [importLine] = replacedContent.match(reg);
+    return {name, importLine};
+  });
 
-    // create declaration file path and use it as object key
-    const filePath = path.relative(`${__dirname}/${SRC}`, file.dirname);
-    const declarationFileName = `${file.stem}.d.ts`;
+  // create declaration file path and use it as object key
+  const filePath = path.relative(`${__dirname}/${SRC}`, file.dirname);
+  const declarationFileName = `${file.stem}.d.ts`;
 
-    // set to the global object map
-    dependenciesMap[`${filePath}/${declarationFileName}`] = parsed;
+  // set to the global object map
+  dependenciesMap[`${filePath}/${declarationFileName}`] = parsed;
 }
 
 function fixDeclarationFile(file, filePath) {
-    // remove indexed property ([x: string]: any), revert constructor changes
-    let replacedContent = String(file.contents)
-        .replace(INDEXED_PROPERTY_REG, '')
-        .replace(new RegExp(ANY_CONSTRUCTOR), LIT_CONSTRUCTOR);
+  // remove indexed property ([x: string]: any), revert constructor changes
+  let replacedContent = String(file.contents)
+    .replace(INDEXED_PROPERTY_REG, '')
+    .replace(new RegExp(ANY_CONSTRUCTOR), LIT_CONSTRUCTOR);
 
-    let imports = LIT_IMPORT;
-    const types = [];
-    // add additional imports and typings for dependencies if exists
-    dependenciesMap[filePath].forEach(({importLine, name}) => {
-        imports += `${importLine}\n`;
-        types.push(`ReturnType<typeof ${name}>`);
-    });
-    if (types.length) {
-        replacedContent = replacedContent.replace(/(} & T)/, `$1 & ${types.join(' & ')}`);
-    }
-    file.contents = Buffer.from(`${imports}${replacedContent}`);
+  let imports = LIT_IMPORT;
+  const types = [];
+  // add additional imports and typings for dependencies if exists
+  dependenciesMap[filePath].forEach(({importLine, name}) => {
+    imports += `${importLine}\n`;
+    types.push(`ReturnType<typeof ${name}>`);
+  });
+  if (types.length) {
+    replacedContent = replacedContent.replace(/(} & T)/, `$1 & ${types.join(' & ')}`);
+  }
+  file.contents = Buffer.from(`${imports}${replacedContent}`);
 }
