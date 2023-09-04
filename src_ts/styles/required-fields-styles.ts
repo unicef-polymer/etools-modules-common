b7cf4ee84d58b6ea9a0b9331e20506ade7c753db
Trif Adriana <trif.corina.adriana@gmail.com>
2023-09-04 19:40:21 +0300

    Required styles update

--- src_ts/styles/required-fields-styles.ts
+++ src_ts/styles/required-fields-styles.ts
@@ -1,13 +1,20 @@
 import {html} from 'lit';
-
 export const RequiredFieldsStyles = html`<style>
-  paper-input[required][label],
-  paper-textarea[required][label],
-  paper-input-container[required],
+  label[required] {
+    background: url('./images/required.svg') no-repeat 87% 40%/6px;
+    width: auto !important;
+    max-width: 100%;
+    right: auto;
+    padding-inline-end: 15px;
+  }
+  :host-context([dir='rtl']) *[required] {
+    background: url('./images/required.svg') no-repeat 0 20%/8px;
+    right: auto;
+    padding-inline-end: 15px;
+  }
+
   datepicker-lite[required],
-  etools-upload[required],
-  etools-currency-amount-input[required],
-  paper-textarea-with-icon[required] {
+  etools-upload[required] {
     --paper-input-container-label: {
       @apply --required-star-style;
       color: var(--secondary-text-color, #737373);
@@ -20,9 +27,5 @@ export const RequiredFieldsStyles = html`<style>
   etools-dropdown-multi[required]::part(esmm-label),
   etools-dropdown[required]::part(esmm-label) {
     @apply --required-star-style;
-  }
-  label[required] {
-    @apply --required-star-style;
-    background: url('./images/required.svg') no-repeat 87% 40%/6px;
   }
 </style>`;
